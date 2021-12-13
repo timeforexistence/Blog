12ef9eab4e77f5f354ee9b667697b1ea7af0ac2d
timeforexistence <96051672+timeforexistence@users.noreply.github.com>
2021-12-13 20:43:57 +0500

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! За ключ не переживаю, не прод.
 SECRET_KEY = 'django-insecure-*h45mln9rw8w4-3typfwb7mlu$l#r+3iwx)4z)ttw&$y9ezk+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
